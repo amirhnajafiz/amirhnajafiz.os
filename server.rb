6b4def9f87d413cb4cef0b264e1d7eda6a43c7c5
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-04 14:09:29 +0330

    server locating different clients

--- server.rb
+++ server.rb
@@ -6,13 +6,16 @@ require 'socket'
 server = TCPServer.open(2000)  # Opening the server socket
 puts "Listening on port 2000 ...."
 
+i = 0
+
 loop {
     client = server.accept  # Waiting for client to join
-    puts "Client #{client} accepted"
+    i += 1
+    puts "Client AASS#{i} accepted"
     client.puts(Time.now.ctime)
     client.puts("Closing the connection by server!")
     client.close
-    puts "Client #{client} disconncted !"
+    puts "Client AASS#{i} disconncted !"
 }
 
 puts "Server terminated !"
