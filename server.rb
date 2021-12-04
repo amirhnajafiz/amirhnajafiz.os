3c3b12815225e3cbd48adca70acda69fe602f603
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-04 13:54:15 +0330

    single client server added

--- server.rb
+++ server.rb
@@ -1,3 +1,16 @@
 #!/usr/bin/env ruby
 
 # Implementing a server in ruby programming language.
+require 'socket'
+
+server = TCPServer.open(2000)  # Opening the server socket
+puts "Listening on port 2000 ...."
+
+loop {
+    client = server.accept  # Waiting for client to join
+    client.puts(Time.now.ctime)
+    client.puts("Closing the connection by server!")
+    client.close
+}
+
+puts "Server terminated !"
